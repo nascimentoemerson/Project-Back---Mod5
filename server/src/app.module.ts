ffd82472d12d6424e5a24f5fc846fa76918bc04f
Emerson Nascimento <106358298+nascimentoemerson@users.noreply.github.com>
2022-11-26 20:25:14 -0300

    feat: create database module and fix user repository

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from './prisma/database.module';
 import { UserService } from './user/services/user.service';
 import { Usercontroller } from './user/user.controller';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [Usercontroller],
   providers: [UserService],
 })
--- server/src/user/user.repository.ts
+++ server/src/user/user.repository.ts
@@ -1,7 +1,40 @@
+import { IUserEntity } from './entities/user.entity';
+import { PartialUserInputDTO } from './services/dto/partialUserInput.dto';
+import { UserInputDTO } from './services/dto/userInput.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
 export class UserRepository {
-    constructor(private readonly user: User) {}
-    
-    async findUserById(id: number): Promise<User> {
-        return this.user.findOne(id);
-    }
+  constructor(private prisma: PrismaService) {}
+
+  async createUser(user: UserInputDTO): Promise<IUserEntity> {
+    const CreateUser = await this.prisma.user.create({ data: user });
+    return CreateUser;
+  }
+
+  async updateUser(user: PartialUserInputDTO): Promise<IUserEntity> {
+    const UpdateUser = await this.prisma.user.update({
+      where: { id: user.id },
+      data: user,
+    });
+    return UpdateUser;
+  }
+
+  async deleteUser(id: string): Promise<IUserEntity> {
+    const DeleteUser = await this.prisma.user.delete({
+      where: { id: id },
+    });
+    return DeleteUser;
+  }
+
+  async findAllUser(): Promise<IUserEntity> {
+    const AllUser = await this.prisma.user.findMany();
+    return AllUser;
+  }
+
+  async findUserById(id: string): Promise<IUserEntity> {
+    const UserById = await this.prisma.user.findUniqueOrThrow({
+      where: { id: id },
+    });
+    return UserById;
+  }
 }
