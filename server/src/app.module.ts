d51074e6e9cce3cc9f9c0243f83e56a6a949dcd4
Emerson Nascimento <106358298+nascimentoemerson@users.noreply.github.com>
2022-12-10 18:20:45 -0300

    feat create generate movies and profie

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,11 @@ import { DatabaseModule } from './prisma/database.module';
 import { UserService } from './user/services/user.service';
 import { Usercontroller } from './user/user.controller';
 import { UserRepository } from './user/user.repository';
+import { MoviesModule } from './movies/movies.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, MoviesModule, ProfileModule],
   controllers: [Usercontroller],
   providers: [UserService, UserRepository],
 })
