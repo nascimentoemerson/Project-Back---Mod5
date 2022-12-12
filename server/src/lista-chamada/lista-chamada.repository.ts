cf49b88573750f7691a4450a63b7ef24d35c01eb
Emerson Nascimento <106358298+nascimentoemerson@users.noreply.github.com>
2022-12-12 20:03:45 -0300

    feat: create lista chamada repository

--- server/src/lista-chamada/lista-chamada.repository.ts
+++ server/src/lista-chamada/lista-chamada.repository.ts
@@ -1,5 +1,27 @@
 import { PrismaService } from 'src/prisma/prisma.service';
+import { ListaChamada } from './lista-chamada.entity';
 
 export class ListaChamadaRepository {
   constructor(private readonly prismaService: PrismaService) {}
-}
+
+  async criarListaChamada({
+    salaID,
+    dia,
+    dataFinal,
+    id,
+    dataInicial,
+    estudantes,
+  }: ListaChamada): Promise<ListaChamada> {
+    return await this.prismaService.listaChamada.create({
+      data: {
+        dia: dia,
+        dataFinal: dataFinal,
+        id: id,
+        dataInicial: dataInicial,
+        salaID: salaID,
+      },
+      include: {
+        estudantes: true,
+      },
+    });
+  }
